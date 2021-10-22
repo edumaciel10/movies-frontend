107d2ee7b5465823563700e5a616643fde96ef27
Eduardo Maciel de Matos <mrgamex49@gmail.com>
2021-10-22 00:22:38 -0300

    feat(init): ༼つ◕_◕༽つ initial commit

---
+++ movies/src/App.js
@@ -0,0 +1,52 @@
+// your-app-name/src/App.js
+import React from 'react';
+import './App.css';
+import fetchGraphQL from './fetchGraphQL';
+
+const { useState, useEffect } = React;
+
+function App() {
+  // We'll load the name of a repository, initially setting it to null
+  const [name, setName] = useState(null);
+
+  // When the component mounts we'll fetch a repository name
+  useEffect(() => {
+    let isMounted = true;
+    fetchGraphQL(`
+      query getMovies { 
+        movies {
+          name,
+        }
+      }
+    `).then(response => {
+      // Avoid updating state if the component unmounted before the fetch completes
+      if (!isMounted) {
+        return;
+      }
+      const data = response.data;
+      console.log(data.movies);
+      setName(data.movies);
+    }).catch(error => {
+      console.error(error);
+    });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [fetchGraphQL]);
+
+  // Render "Loading" until the query completes
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          {
+            name.map((value) => `Filme: ${value.name}`)
+          }
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
